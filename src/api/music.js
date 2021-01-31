485ad19b09048b3f0dfafcb8af06be2b4e36dd28
贾顺 <jiashun@outlook.com>
2021-01-31 10:59:12 +0800

    add music

--- src/api/music.js
+++ src/api/music.js
@@ -1,40 +1,47 @@
 import request from '@/utils/request'
 
-export function getAllJob() {
+export function get() {
   const params = {
     page: 0,
-    size: 9999,
-    enabled: true
+    pageSize: 20
   }
   return request({
-    url: 'api/job',
+    url: 'music',
     method: 'get',
     params
   })
 }
 
+export function getById(id) {
+  return request({
+    url: 'music/id',
+    method: 'post',
+    data: id
+  })
+}
+
 export function add(data) {
   return request({
-    url: 'api/job',
+    url: 'music',
     method: 'post',
     data
   })
 }
 
-export function del(ids) {
+export function del(id) {
   return request({
-    url: 'api/job',
+    url: 'music',
     method: 'delete',
-    data: ids
+    data: id
   })
 }
 
 export function edit(data) {
   return request({
-    url: 'api/job',
+    url: 'music',
     method: 'put',
     data
   })
 }
 
-export default { add, edit, del }
+export default { getById, add, edit, del }
